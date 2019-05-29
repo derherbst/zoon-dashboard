d4b10a3800d6b310d9c2db1084beb140a75de092
Georgy Gerbst <g.gerbst@it.ecommpay.com>
2019-05-29 12:22:35 +0300

    Updates

---
+++ precache-manifest.0aa0f4e1a199161f28ad6d22882c888a.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa36220c850104d2dfa979e0c268c67a",
+    "url": "/zoon-dashboard/index.html"
+  },
+  {
+    "revision": "896a200248273a31cbbf",
+    "url": "/zoon-dashboard/static/css/main.a1a19bb6.chunk.css"
+  },
+  {
+    "revision": "25d0ae321b5ea7a0735b",
+    "url": "/zoon-dashboard/static/js/2.e9f319c9.chunk.js"
+  },
+  {
+    "revision": "896a200248273a31cbbf",
+    "url": "/zoon-dashboard/static/js/main.ca140929.chunk.js"
+  },
+  {
+    "revision": "2f7e1bfc2ad4dcf3cc14",
+    "url": "/zoon-dashboard/static/js/runtime~main.f6658613.js"
+  },
+  {
+    "revision": "59a81f5848adfd48704f7686f61d294d",
+    "url": "/zoon-dashboard/static/media/Roboto-Bold.59a81f58.woff"
+  },
+  {
+    "revision": "7c18188784f21915f42a5b3bc9d91e20",
+    "url": "/zoon-dashboard/static/media/Roboto-Bold.7c181887.ttf"
+  },
+  {
+    "revision": "8d2e5e08838fb228bcd3812467b28593",
+    "url": "/zoon-dashboard/static/media/Roboto-Bold.8d2e5e08.eot"
+  },
+  {
+    "revision": "e48436b74b55201a3edfa95d682fce86",
+    "url": "/zoon-dashboard/static/media/Roboto-Bold.e48436b7.svg"
+  },
+  {
+    "revision": "721e14e4c7b008cc218f6f5e163e9158",
+    "url": "/zoon-dashboard/static/media/Roboto-Light.721e14e4.woff"
+  },
+  {
+    "revision": "9767f01ca4cbd4eb66707e7ce247c9cd",
+    "url": "/zoon-dashboard/static/media/Roboto-Light.9767f01c.eot"
+  },
+  {
+    "revision": "c56e7c41ec32c76758cc7ac4935cb57a",
+    "url": "/zoon-dashboard/static/media/Roboto-Light.c56e7c41.svg"
+  },
+  {
+    "revision": "c6cdfded4630ba6d9a2dceb70aa4fe0f",
+    "url": "/zoon-dashboard/static/media/Roboto-Light.c6cdfded.ttf"
+  },
+  {
+    "revision": "1ae87273a97dec09ee922329ff3b7b63",
+    "url": "/zoon-dashboard/static/media/Roboto-Medium.1ae87273.woff"
+  },
+  {
+    "revision": "c6eba7a9564c145a8ad3372c3bde8678",
+    "url": "/zoon-dashboard/static/media/Roboto-Medium.c6eba7a9.eot"
+  },
+  {
+    "revision": "d52f011be65b281ba8ca1c3f689cf133",
+    "url": "/zoon-dashboard/static/media/Roboto-Medium.d52f011b.ttf"
+  },
+  {
+    "revision": "ea9faa5d7371550be3894d7363531f6f",
+    "url": "/zoon-dashboard/static/media/Roboto-Medium.ea9faa5d.svg"
+  },
+  {
+    "revision": "18d44f79b3979ec168862093208c6d7d",
+    "url": "/zoon-dashboard/static/media/Roboto-Regular.18d44f79.ttf"
+  },
+  {
+    "revision": "22b3a8a99a350a62c9c0d1dc0e3e17e9",
+    "url": "/zoon-dashboard/static/media/Roboto-Regular.22b3a8a9.eot"
+  },
+  {
+    "revision": "63779c25dc172b238eb12a41aaf2753c",
+    "url": "/zoon-dashboard/static/media/Roboto-Regular.63779c25.svg"
+  },
+  {
+    "revision": "920a9d16371d0859c82d0a24b7bda6b3",
+    "url": "/zoon-dashboard/static/media/Roboto-Regular.920a9d16.woff"
+  },
+  {
+    "revision": "c961ea79ad3bd3fbe1ebabc7316f4c2b",
+    "url": "/zoon-dashboard/static/media/i_fill.c961ea79.svg"
+  },
+  {
+    "revision": "76d93e47d4fe29212f4e34e2e6a93a6b",
+    "url": "/zoon-dashboard/static/media/i_rate.76d93e47.svg"
+  },
+  {
+    "revision": "d9095bec23d340de009766591f94592b",
+    "url": "/zoon-dashboard/static/media/i_refresh.d9095bec.svg"
+  },
+  {
+    "revision": "da975cd92293751aa910df52c65ebf2d",
+    "url": "/zoon-dashboard/static/media/i_refresh2.da975cd9.svg"
+  },
+  {
+    "revision": "960d62fc1ea044f4b192112e7e4acaac",
+    "url": "/zoon-dashboard/static/media/i_review.960d62fc.svg"
+  },
+  {
+    "revision": "a0e9cb2460c8500a00686f5e85588283",
+    "url": "/zoon-dashboard/static/media/i_review2.a0e9cb24.svg"
+  },
+  {
+    "revision": "11b16b975c8441d49a28ca596141523f",
+    "url": "/zoon-dashboard/static/media/i_warn.11b16b97.svg"
+  }
+]);
